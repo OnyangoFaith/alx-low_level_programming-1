27408bd22cec522e7be96768044e3fa4c54182c2
MeFerdi <ferdinandodhis254@gmail.com>
2022-12-08 09:53:14 -0800

    my first c programming files

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,12 @@
-#!/bin/bash
+
 #include <stdio.h>
 
 /**
- * main - Entry point
- *
- * Return: Always 0 (success)
+ * main - prints the phrase "Programming is like building multilingual puzzle
+ * Return: 0 if exited properly, non-zero otherwise
  */
-
-int main(void)
+ int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }
